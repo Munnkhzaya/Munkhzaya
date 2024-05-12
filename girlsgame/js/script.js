7e689a275b35033bd5951fb9bbbe76df2f511505
Munnkhzaya <168347221+Munnkhzaya@users.noreply.github.com>
2024-05-12 09:30:41 +0800

    Add files via upload

---
+++ girlsgame/js/script.js
@@ -0,0 +1,12 @@
+function bage() {
+    document.getElementById("result").src="img/bage.jpg";
+}
+
+function pink() {
+    document.getElementById("result").src="img/brown.jpg";
+}
+
+function brown() {
+    document.getElementById("result").src="img/pink.jpg";
+}
+
